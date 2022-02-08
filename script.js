7dc7ab565a1aa2bfd5be4d07191f471bc8dab536
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-08 23:10:49 +0500

    Maps

--- script.js
+++ script.js
@@ -38,30 +38,60 @@ const restaurant = {
   },
 };
 
+//// --------------- MAPS --------------- ////
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+console.log(rest);
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+console.log(rest.delete(2));
+console.log(rest.size);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.get(arr);
+
 //// --------------- SETS --------------- ////
 
-const ordersSet = new Set(['Pasta', 'Pitsa', 'Pitsa', 'Risotto', 'Pasta']);
-console.log(ordersSet);
-console.log(new Set('Jonas'));
-console.log(ordersSet.size);
-console.log(ordersSet.has('Bread'));
-console.log(ordersSet.has('Pitsa'));
-console.log(ordersSet.add('Garlic Bread'));
-console.log(ordersSet.add('Garlic Bread'));
-console.log(ordersSet.delete('Pasta'));
-console.log(ordersSet);
-// ordersSet.clear()
-for (const order of ordersSet) console.log(order);
+// const ordersSet = new Set(['Pasta', 'Pitsa', 'Pitsa', 'Risotto', 'Pasta']);
+// console.log(ordersSet);
+// console.log(new Set('Jonas'));
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Bread'));
+// console.log(ordersSet.has('Pitsa'));
+// console.log(ordersSet.add('Garlic Bread'));
+// console.log(ordersSet.add('Garlic Bread'));
+// console.log(ordersSet.delete('Pasta'));
+// console.log(ordersSet);
+// // ordersSet.clear()
+// for (const order of ordersSet) console.log(order);
 
-// Example
+// // Example
 
-const staff = ['Waiter', 'Sheff', 'Waiter', 'Manager', 'Sheff', 'Waiter'];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
-console.log(
-  new Set(['Waiter', 'Sheff', 'Waiter', 'Manager', 'Sheff', 'Waiter']).size
-);
-console.log(new Set('Jonas').size);
+// const staff = ['Waiter', 'Sheff', 'Waiter', 'Manager', 'Sheff', 'Waiter'];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(
+//   new Set(['Waiter', 'Sheff', 'Waiter', 'Manager', 'Sheff', 'Waiter']).size
+// );
+// console.log(new Set('Jonas').size);
 
 //// --------------- ASSIGNMENT 2 --------------- ////
 
