d35262e26329a4127d5532623494166d912249b6
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-08 21:50:37 +0500

    Looping objects

--- script.js
+++ script.js
@@ -38,33 +38,57 @@ const restaurant = {
   },
 };
 
+//// --------------- LOOPING OBJECTS --------------- ////
+// Property names
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property values
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and ${close}`);
+}
+
 //// --------------- OPTIONAL CHAINING --------------- ////
 
-if (restaurant.openingHours && restaurant.openingHours.mon) {
-  console.log(restaurant.openingHours.mon);
-}
-// WITH optional chaining
-console.log(restaurant.openingHours?.mon?.open);
+// if (restaurant.openingHours && restaurant.openingHours.mon) {
+//   console.log(restaurant.openingHours.mon);
+// }
+// // WITH optional chaining
+// console.log(restaurant.openingHours?.mon?.open);
 
-// Example
-const days = ['mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`On ${day} we open at ${open}`);
-}
+// // Example
+// const days = ['mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day} we open at ${open}`);
+// }
 
-// Method
+// // Method
 
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
-// ARRAYS
-// const users = [{ name: 'John', email: 'john@' }];
-const users = [];
-console.log(users[0]?.name ?? 'User array empty');
+// // ARRAYS
+// // const users = [{ name: 'John', email: 'john@' }];
+// const users = [];
+// console.log(users[0]?.name ?? 'User array empty');
 
-if (users.length > 0) console.log(users[0]?.name);
-else console.log('User array empty');
+// if (users.length > 0) console.log(users[0]?.name);
+// else console.log('User array empty');
 
 //// --------------- LOOPING ARRAYS --------------- ////
 
