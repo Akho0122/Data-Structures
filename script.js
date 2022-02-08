fd0e621a1fec80d17b28c06b41a62b2354d2cda7
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-08 22:14:39 +0500

    Assignment 2

--- script.js
+++ script.js
@@ -37,30 +37,93 @@ const restaurant = {
     console.log(mainIngredient, otherIngredients);
   },
 };
+//// --------------- ASSIGNMENT 2 --------------- ////
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1.
+
+for (const [i, el] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${el}`);
+}
+
+// 2.
+
+let average = 0;
+const odds = Object.values(game.odds);
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
 
 //// --------------- LOOPING OBJECTS --------------- ////
-// Property names
-const properties = Object.keys(openingHours);
-console.log(properties);
 
-let openStr = `We are open on ${properties.length} days: `;
+// // Property names
+// const properties = Object.keys(openingHours);
+// console.log(properties);
 
-for (const day of properties) {
-  openStr += `${day}, `;
-}
-console.log(openStr);
+// let openStr = `We are open on ${properties.length} days: `;
 
-// Property values
-const values = Object.values(openingHours);
-console.log(values);
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
 
-// Entire object
-const entries = Object.entries(openingHours);
-console.log(entries);
+// // Property values
+// const values = Object.values(openingHours);
+// console.log(values);
 
-for (const [key, { open, close }] of entries) {
-  console.log(`On ${key} we open at ${open} and ${close}`);
-}
+// // Entire object
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open} and ${close}`);
+// }
 
 //// --------------- OPTIONAL CHAINING --------------- ////
 
