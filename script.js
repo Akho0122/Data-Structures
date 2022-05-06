fe12503abd71a272b3da26e0f2b06569ad782cb9
Akho0122 <mr.akhror.dev@gmail.com>
2022-05-06 19:16:00 +0500

    Data structures part 2

--- script.js
+++ script.js
@@ -28,20 +28,186 @@ const restaurant = {
       to ${address} at ${time}`
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
+  orderPizza: function (mainIng, ...otherIngs) {
+    console.log(mainIng, otherIngs);
+  },
 };
+
+////////////////// FOR OF LOOP ////////////////////
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+for (const item of menu) {
+  cl;
+}
+
+////////////////// ASSIGNMENT 1 ////////////////////
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// // Task 1.
+// const [player1, player2] = game.players;
+// console.log(player1, player2);
+
+// // Task 2.
+// const [gk, ...fieldPlayers] = player1;
+// console.log(gk, fieldPlayers);
+
+// // Task 3.
+// const [allPlayers] = [...player1, player2];
+// console.log(allPlayers);
+
+// // Task 4.
+// const [playersFinal] = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(playersFinal);
+
+// // Task 5.
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// // Task 6.
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored`);
+// };
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+
+// // Task 7.
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team2 < team1 && console.log('Team 2 is more likely to win');
+
+////////////////// SHORT CIRCUITING //////////////////
+
+// const rest1 = {
+//   name: 'Capri',
+//   numGuests: 0,
+// };
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Akho',
+// };
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// console.log(rest1);
+// console.log(rest2);
+
+////////////////// LOGICAL ASSIGNMENT //////////////////
+
+// console.log(3 || 'Jonas');
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// restaurant.numGuests = 0;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+// const guests2 = restaurant.numGuests ?? 10;
+// console.log(guests2);
+
+// console.log(0 && 'jonas');
+// console.log(7 && 'jonas');
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+////////////////// REST OPERATOR //////////////////
+
+// const arr = [1, 2, ...[3, 4]];
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
+
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(sat, weekdays);
+
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(7, 8, 9, 1, 2, 4);
+// const x = [23, 5, 7];
+// add(...x);
+// restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
 
 ////////////////// SPREAD OPERATOR ////////////////
 
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(newMenu);
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
 
-const mainMenuCopy = [...restaurant.mainMenu];
-const joinedMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log(joinedMenu);
+// const mainMenuCopy = [...restaurant.mainMenu];
+// const joinedMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(joinedMenu);
 
-const str = 'Jonas';
-const letters = [...str, '', 'S.'];
-console.log(letters);
+// const str = 'Jonas';
+// const letters = [...str, '', 'S.'];
+// console.log(letters);
+
+// // const ingredients = [
+// //   prompt("Let's make pasta! Ingredient 1?"),
+// //   prompt('Ingredient 2?'),
+// //   prompt('Ingredient 3?'),
+// // ];
+// // restaurant.orderPasta(...ingredients);
+// const newRestaurant = { ...restaurant, founder: 'Akho', foundedIn: 2002 };
+// console.log(newRestaurant);
 
 ////////////////// OBJECT DESTRUCTURING ////////////////
 
