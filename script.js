19e6c0d750c2ba72ffc933edb5a274addb582487
Akho0122 <mr.akhror.dev@gmail.com>
2022-05-08 14:33:18 +0500

    string

--- script.js
+++ script.js
@@ -5,46 +5,220 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: { open: 12, close: 22 },
+  [weekdays[4]]: { open: 11, close: 23 },
+  [weekdays[5]]: { open: 0, close: 24 },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
 
-  openingHours: {
-    thu: { open: 12, close: 22 },
-    fri: { open: 11, close: 23 },
-    sat: { open: 0, close: 24 },
-  },
-
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+  orderDelivery({ starterIndex, mainIndex, time, address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]}
       and ${this.mainMenu[mainIndex]} will be delivered
       to ${address} at ${time}`
     );
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
     );
   },
-  orderPizza: function (mainIng, ...otherIngs) {
+  orderPizza(mainIng, ...otherIngs) {
     console.log(mainIng, otherIngs);
   },
 };
 
+////////////////// WORKING WITH STRINGS ////////////////////
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+const checkMiddleSeat = function (seat) {
+  const last = seat.slice(-1);
+  // console.log(last);
+  if (last === 'B' || last === 'E') {
+    console.log('You got the middle seat');
+  } else {
+    console.log('You got lucky');
+  }
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('3C');
+
+const passenger = 'jOnAs';
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+const email = 'hello@example.com';
+const loginEmail = ' Hello@Examlple.Io';
+
+const lowerEmail = loginEmail.toLowerCase();
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+
+const priceGB = '288,97&';
+const priceUS = priceGB.replace('&', '$').replace(',', '.');
+console.log(priceUS);
+
+const plane2 = 'A32neo';
+console.log(plane2.includes('A320'));
+
+const chekcBaggage = function (items) {
+  const item = items.trim().toLowerCase();
+  if (item.includes('knife') || item.includes('gun')) {
+    console.log('You are not allowed on board');
+  } else {
+    console.log('Welcome a board');
+  }
+};
+chekcBaggage('I have laptop, some food and a pocket knife');
+chekcBaggage(' Have some socks and camera');
+chekcBaggage(' Got some snack and a gun for selprotection');
+
+const capitalizeName = function (name) {
+  const lower = name.toLowerCase().split(' ');
+  const namesUpper = [];
+  for (const letter of lower) {
+    namesUpper.push(letter.replace(letter[0], letter[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+capitalizeName('jeSSica aNn smitH dAvIs');
+capitalizeName('aKhRor oriBjoNoV');
+
+////////////////// ASSIGNMENT 2 ////////////////////
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// // Task 1.
+// for (const [key, value] of game.scored.entries()) {
+//   console.log(`Goal ${key + 1}: ${value}`);
+// }
+
+// // Task 2.
+// let averageOdd = 0;
+// for (const odd of Object.values(game.odds)) {
+//   averageOdd += odd / 3;
+// }
+// console.log(averageOdd);
+
+// // Task 3.
+// for (const [team, odds] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+//   console.log(`Odd of ${teamStr}: ${odds}`);
+// }
+
+// // Task 4.
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// }
+// console.log(scorers);
+
+////////////////// LOOPING OBJECTS /////////////////
+
+// const properties = Object.keys(openingHours);
+// let openStr = `We are open on ${properties.length}days: `;
+// for (const day of Object.keys(openingHours)) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
+
+// const entries = Object.entries(openingHours);
+// // console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open}, and close at ${close}`);
+// }
+
+////////////////// OPTIONAL CHAINING /////////////////
+
+// console.log(restaurant.openingHours?.mon?.open);
+
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
 ////////////////// FOR OF LOOP ////////////////////
 
-const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
 
-for (const item of menu) {
-  cl;
-}
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
 
 ////////////////// ASSIGNMENT 1 ////////////////////
 
